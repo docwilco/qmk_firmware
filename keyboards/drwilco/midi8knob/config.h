2b114f99e919aa9d3bee4e6e21a50cf7471c4cb2
DocWilco <github@drwil.co>
2026-03-16 14:03:33 +0100

    midi8knob: switch MCU from RP2040 to STM32G431
    - Update processor and bootloader in keyboard.json
    - Add PWM WS2812 config (TIM3_CH2 on PA7, AF2, DMAMUX)
    - Add halconf.h (HAL_USE_PWM) and mcuconf.h (TIM3)
    - Set all encoder/matrix pins to NO_PIN (pending PCB design)
    - Change WS2812 driver from vendor to pwm, pin from GP16 to A7

--- keyboards/drwilco/midi8knob/config.h
+++ keyboards/drwilco/midi8knob/config.h
@@ -4,3 +4,10 @@
 #pragma once
 
 #define MIDI_ADVANCED
+
+// PWM WS2812 configuration
+// PA7 = TIM3_CH2 (AF2)
+#define WS2812_PWM_DRIVER PWMD3
+#define WS2812_PWM_CHANNEL 2
+#define WS2812_PWM_PAL_MODE 2
+#define WS2812_PWM_DMAMUX_ID STM32_DMAMUX1_TIM3_UP
